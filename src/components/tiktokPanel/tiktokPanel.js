d25022c431aebb619136e483fe1b8826beed22a8
Swaroop Gupta Voleti <vswaroop@athenahealth.com>
2020-09-26 13:30:30 +0530

    installed https://www.npmjs.com/package/react-easy-tabs-component for tabs

--- src/components/tiktokPanel/tiktokPanel.js
+++ src/components/tiktokPanel/tiktokPanel.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react'
 import './tiktokPanel.css'
-import Tabs from '../Tabs/Tabs'
-import Panel from '../Tabs/Panel'
 import TitkTokVideos from '../titkTokVideos/titkTokVideos'
 import UserProfile from '../loggedInUserProfile/loggedInUserProfile'
 import Notifications from '../notifications/notifications'
 import CreateVideo from '../videoCreator/videoCreator'
+import EasyTabs from 'react-easy-tabs-component'
 
 export default class tiktokPanel extends Component {
   render() {
     return (
       <section className="tiktok-panel">
-        <Tabs selected={0}>
-          <Panel dataIcon="fas fa-home" title="Home"><TitkTokVideos /></Panel>
-          <Panel dataIcon="fas fa-search" title="Discover"><TitkTokVideos /></Panel>
-          <Panel dataIcon="fas fa-plus-square" title=""><CreateVideo /></Panel>
-          <Panel dataIcon="fas fa-inbox" title="Inbox"><Notifications /></Panel>
-          <Panel dataIcon="fas fa-user" title="Me"><UserProfile /></Panel>
-        </Tabs>
+        <EasyTabs selected={0}>
+          <section dataicon="fas fa-home" title="Home"><TitkTokVideos /></section>
+          <section dataicon="fas fa-search" title="Discover"><TitkTokVideos /></section>
+          <section dataicon="fas fa-plus-square" title=""><CreateVideo /></section>
+          <section dataicon="fas fa-inbox" title="Inbox"><Notifications /></section>
+          <section dataicon="fas fa-user" title="Me"><UserProfile /></section>
+        </EasyTabs>
       </section>
     )
   }
